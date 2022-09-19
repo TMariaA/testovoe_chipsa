47a7bcf0e69d74e61d89d75059447bf47b562025
TMariaA <phoenix_ignea@mail.ru>
2022-09-20 00:07:57 +0300

    add migrations for installation infoblock,add xml file (for inst infoblock )

---
+++ local/php_interface/migrations/Test_Chipsa_cat20220920000309.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Test_Chipsa_cat20220920000309 extends Version
+{
+    protected $description = "миграция категорий инфоблока(разделы новостей)";
+
+    protected $moduleVersion = "4.1.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+
+        $iblockId = $helper->Iblock()->getIblockIdIfExists(
+            'news_chipsa',
+            'news'
+        );
+
+        $helper->Iblock()->addSectionsFromTree(
+            $iblockId,
+            array (
+  0 => 
+  array (
+    'NAME' => 'машины',
+    'CODE' => 'cars',
+    'SORT' => '500',
+    'ACTIVE' => 'Y',
+    'XML_ID' => '1',
+    'DESCRIPTION' => '',
+    'DESCRIPTION_TYPE' => 'text',
+  ),
+  1 => 
+  array (
+    'NAME' => 'животные',
+    'CODE' => 'animal',
+    'SORT' => '500',
+    'ACTIVE' => 'Y',
+    'XML_ID' => '2',
+    'DESCRIPTION' => '',
+    'DESCRIPTION_TYPE' => 'text',
+  ),
+  2 => 
+  array (
+    'NAME' => 'пк и комплектующие',
+    'CODE' => 'tech',
+    'SORT' => '500',
+    'ACTIVE' => 'Y',
+    'XML_ID' => '3',
+    'DESCRIPTION' => '',
+    'DESCRIPTION_TYPE' => 'text',
+  ),
+)        );
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
