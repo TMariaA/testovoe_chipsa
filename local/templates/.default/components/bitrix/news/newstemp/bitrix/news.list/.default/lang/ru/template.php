f37e5a9a577f1927355ad8e26b85d7c038d19f61
TMariaA <phoenix_ignea@mail.ru>
2022-09-22 07:54:05 +0300

    fix mistake in arr MESS

--- local/templates/.default/components/bitrix/news/newstemp/bitrix/news.list/.default/lang/ru/template.php
+++ local/templates/.default/components/bitrix/news/newstemp/bitrix/news.list/.default/lang/ru/template.php
@@ -1,6 +1,6 @@
 <?
 $MESS["SORT_NAME_ASC"] = "по имени(А-Я)";
-$MESS["SORT_NAME_ASC"] = "по имени(Я-А)";
+$MESS["SORT_NAME_DESC"] = "по имени(Я-А)";
 $MESS["SORT_DATE_DESC"] = "по дате создания(по убыванию)";
 $MESS["SORT_DATE_ASC"] = "по дате создания(по возрастанию)";
 $MESS["SORT"] = "Сортировка:";
